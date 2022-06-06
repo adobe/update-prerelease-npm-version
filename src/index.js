20b5e768b761abe236fdbbb611eff2166ba4f65c
Shazron Abdullah <shaz@adobe.com>
2022-06-06 17:09:09 +0800

    fix: filter empty strings from dependencies to update csv

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const { getPackageJson, writePackageJson, generatePrereleaseVersion } = require(
 
 const preReleaseTag = core.getInput('pre-release-tag')
 const packageJsonPath = core.getInput('package-json-path')
-const dependenciesToUpdate = core.getInput('dependencies-to-update').trim().split(',')
+const dependenciesToUpdate = core.getInput('dependencies-to-update').trim().split(',').filter(d => d) // filter empty strings
 const dependenciesToUpdateVersionTag = core.getInput('dependencies-to-update-version-tag')
 const packageName = core.getInput('package-name')
 const shaHash = github.context.sha
